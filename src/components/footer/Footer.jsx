d33516725839d530c11d2aabcab0f3d6e132d919
Marcelo <65622007+marcelovasan@users.noreply.github.com>
2022-02-04 15:13:39 -0500

    correcion de footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return <footer class="footer-distributed">
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-        <div class="card bg-dark" id='cards'>
+        <div class="footer-left" id='cards'>
             <p class="card-header">
                 <a type="button" class="btn btn-dark" href="#"><img></img>Home</a>
                 <a type="button" class="btn btn-dark" href="https://www.facebook.com/VasanMarcelo">Facebook</a>
